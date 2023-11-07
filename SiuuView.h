2274768c4c4250e05cbb09970c0f056e7ca7c7ba
권윤호 <yoonhoktx@gmail.com>
2023-11-07 14:11:36 +0900

    초기 커밋.

---
+++ SiuuView.h
@@ -0,0 +1,63 @@
+﻿
+// SiuuView.h: CSiuuView 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CSiuuView : public CFormView
+{
+protected: // serialization에서만 만들어집니다.
+	CSiuuView() noexcept;
+	DECLARE_DYNCREATE(CSiuuView)
+
+public:
+#ifdef AFX_DESIGN_TIME
+	enum{ IDD = IDD_SIUU_FORM };
+#endif
+
+// 특성입니다.
+public:
+	CSiuuDoc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+	virtual void OnInitialUpdate(); // 생성 후 처음 호출되었습니다.
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnPrint(CDC* pDC, CPrintInfo* pInfo);
+
+// 구현입니다.
+public:
+	virtual ~CSiuuView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	CSpinButtonCtrl m_Spin;
+	CEdit m_Edit;
+	CString m_B;
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	afx_msg void OnSelectColor();
+};
+
+#ifndef _DEBUG  // SiuuView.cpp의 디버그 버전
+inline CSiuuDoc* CSiuuView::GetDocument() const
+   { return reinterpret_cast<CSiuuDoc*>(m_pDocument); }
+#endif
+
